1538a59c8418d7614343510e537151ef56ecaedf
aditya0102 <aditya0102.be21@chitkara.edu.in>
2023-04-26 14:48:59 +0530

    updated arrays

---
+++ array/src/find_frequency.java
@@ -0,0 +1,20 @@
+public class find_frequency {
+    public static void main(String[] args){
+        int[] arr = {1,7,4,3,4,1,7};
+        int[] fre = new int[arr.length];
+
+        for(int i = 0;i<arr.length;i++){
+            int count = 0;
+            for(int j =i+1;j<arr.length;j++){
+                if(arr[i]== arr[j]) {
+                    count++;
+                }
+            }
+            fre[i] = count;
+        }
+
+        for(int i =0;i<fre.length;i++){
+            System.out.print(fre[i]+" ");
+        }
+    }
+}
