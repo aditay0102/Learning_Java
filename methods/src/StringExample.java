749d71a4c0e151c4fceb91284c4890f489273472
aditya0102 <aditya0102.be21@chitkara.edu.in>
2023-05-06 22:13:43 +0530

    added methods

---
+++ methods/src/StringExample.java
@@ -0,0 +1,11 @@
+
+public class StringExample {
+
+    static void greet(String name){
+         System.out.println(name);
+    }
+    public static void main(String[] args){
+        String name = "adi";
+        greet(name);
+    }
+};
---
+++ methods/src/sum.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class sum {
+    public static void main(String[] args){
+        int ans = sum2();
+        System.out.println(ans);
+    }
+//    return the value
+    static int sum2(){
+
+        Scanner in  = new Scanner(System.in);
+
+        System.out.println("Enter number 1: ");
+        int num1 = in.nextInt();
+        System.out.println("Enter number 2: ");
+        int num2 = in.nextInt();
+
+        int sum = num1 + num2;
+
+       return sum;
+    }
+static  void sum(){
+        Scanner in  = new Scanner(System.in);
+
+        System.out.println("Enter number 1: ");
+        int num1 = in.nextInt();
+        System.out.println("Enter number 2: ");
+        int num2 = in.nextInt();
+
+        int sum = num1 + num2;
+
+        System.out.println("the sum is "+sum);
+    }
+}
