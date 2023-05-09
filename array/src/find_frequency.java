10a1b3b64a81cca609d81b527326ef8e17ff922b
aditya0102 <aditya0102.be21@chitkara.edu.in>
2023-05-10 01:33:39 +0530

    added frequency finder in java

--- array/src/find_frequency.java
+++ array/src/find_frequency.java
@@ -1,7 +1,13 @@
+import  java.util.*;
 public class find_frequency {
     public static void main(String[] args){
-        int[] arr = {1,7,4,3,4,1,7};
+        int[] arr = {1,7,4,3,4,1,7,1,1};
         int[] fre = new int[arr.length];
+
+        Arrays.sort(arr);`
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
 
         for(int i = 0;i<arr.length;i++){
             int count = 0;
@@ -21,6 +27,6 @@ public class find_frequency {
             System.out.print(fre[i]+" ");
         }
 
-        System.out.print(start)
+
     }
 }
