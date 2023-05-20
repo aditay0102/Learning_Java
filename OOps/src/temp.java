6e23054b9e8b18eb829d87cb8eb13152b366692c
aditya0102 <aditya0102.be21@chitkara.edu.in>
2023-05-20 13:01:16 +0530

    added oops man

--- OOps/src/temp.java
+++ OOps/src/temp.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class temp {
+public class temp {
+    public static void main(String[] args){
+        int[] arr = {};
+        int s = 0;
+        int e = arr.length - 1;
+        System.out.print(" e is "+ e);
+
+        int x = 0 , a = 0 , count = 0 , i = 0;
+        while(s<e){
+            int temp  = arr[a]; // 1
+            a  = a + temp; // 0 + 1 = 1;
+            count++;
+            if(a>=e){
+
+                break;
+            }
+
+
+        }
+       System.out.print(count+" ");
+
+    }
 }
