3240fbd925eed82b63369c5d064994716ac3ac07
aditya0102 <aditya0102.be21@chitkara.edu.in>
2023-05-21 14:47:10 +0530

    updated sort_strings

--- strings/src/sort_string.java
+++ strings/src/sort_string.java
@@ -1,5 +1,30 @@
-public class sort_string {
-    String a = "cba";
 
-    
+import java.util.*;
+
+public class sort_string {
+    public static void main(String[] args){
+        String a = "cba";
+        String b = "bca";
+
+
+        char arr[] = a.toCharArray();
+        char brr[] = b.toCharArray();
+
+
+        Arrays.sort(brr);
+        Arrays.sort(arr);
+
+        String ans = new String(arr);
+        String bns = new String(brr);
+
+        if(ans.equals(bns)){
+            System.out.print("Yes");
+        }
+
+
+        System.out.print(bns);
+    }
+
+
+
 }
