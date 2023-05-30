616ccb0c0d2e00fc7e35edcaaedfa04c5c6c67ae
aditya0102 <aditya0102.be21@chitkara.edu.in>
2023-05-30 10:38:49 +0530

    added linked list

--- linked_list/src/Main.java
+++ linked_list/src/Main.java
@@ -4,11 +4,26 @@ import java.util.LinkedList;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
+    /*       singley linked list
         LL list = new LL();
         list.insertFirst(1);
         list.insertFirst(99);
+        list.insertLast(200);
+
 
         list.display();
+
+        //list.deleteLast();
+        list.deleteindex(1);
+        System.out.println();
+        list.display();
+*/
+
+        DLL l = new DLL();
+        l.insertFirst(1);
+        l.insertFirst(3);
+        l.insertFirst(5);
+        l.displayRev();
 
     }
 }
